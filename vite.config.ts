5d408afbd7c42764046d84c20465d5bd7520480c
Gabriel Possobon <gabrielpossobon1@gmail.com>
2026-08-11 11:32:03 -0300

    Update base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: process.env.FIGMA_PUBLIC_URL ? `${process.env.FIGMA_PUBLIC_URL}/` : '/',
+    base: '/SaaSPricingManagementPlatform/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
